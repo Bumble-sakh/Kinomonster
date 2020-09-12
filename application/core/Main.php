907671b679d21ff9b4f25231170c30fe68c2a287
Bumble-sakh <bumble.sakh@gmial.com>
2020-09-12 12:34:58 +1100

    add MY_Controller

---
+++ application/core/Main.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access alowed');
+
+class Main extends MY_Controller {
+
+    public function __construct() {
+        parent::__construct;
+    }
+
+    public function index() {
+
+        $this->data['title'] = "Главная страница";
+
+        $this->load->view('templates/header', $this->data);
+        $this->load->view('main/index', $this->data);
+        $this->load->view('templates/footer', $this->data);
+
+    }
+
+}
